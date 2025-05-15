fa1e818cfc6de09c2d98fd1cfad44f15bd626fea
Halanau327 <a.halanau@gmail.com>
2025-05-15 23:48:01 +0300

    upd: check repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,6 +86,12 @@ export default function Home() {
             alt="Globe icon"
             width={16}
             height={16}
+          />       <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
           />
           Go to nextjs.org →
         </a>
